c5c3280463f89d370360543aa3a9e6335a998887
Simon van der Weele <simon@rightbrain.com.mt>
2018-02-19 12:38:49 +0100

    moved animation event broadcaster from RPG Core

---
+++ Core/transform/Display/Animation/Events/unity/AnimationEventBroadcaster.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace unity.Helpers.Animations
+{
+    public class AnimationEventBroadcaster : MonoBehaviour
+    {
+        public void OnAnimation(AnimationEvent evt)
+        {
+            BroadcastEvent(evt);
+        }
+
+        public void OnAnimComplete(AnimationEvent evt)
+        {
+            var e = BroadcastEvent(evt);
+            e.isAnimationComplete = true;
+        }
+
+        private GameEntity BroadcastEvent(AnimationEvent evt)
+        {
+            var e = Contexts.sharedInstance.game.GetEntityWithView(gameObject);
+            e.AddAnimationEvent(evt);
+            return e;
+        }
+    }
+}
